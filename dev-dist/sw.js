0ccbcc4172ac2ca5ce2dcad818c74bb409f99fae
IvanxHK <ivanhk815@gmail.com>
2023-11-13 20:48:43 -0600

    second commit

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-5357ef54'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.5u2io37ollo"
+    "revision": "0.qbjrt0k5bi"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
